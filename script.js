b6e53d9be4ce6159e664a1f6e61a4da762c23302
Shay Buzz <shaybuzz@gmail.com>
2020-07-04 15:10:02 +0300

    advertisment

---
+++ script.js
@@ -0,0 +1,19 @@
+var images = [], index = 0, interval = 5000;
+images[0] = "images/adv1.jpg";
+images[1] = "images/adv2.jpg";
+images[2] = "images/adv3.jpg";
+
+window.onload = function () {
+    setInterval(function () { changePic(); }, interval);
+}
+
+function changePic() {
+    index++;
+    if (index == images.length) {
+        index = 0;
+    }
+    var imgAdv = document.getElementById('dynamic');
+
+    imgAdv.src = images[index];
+}
+
