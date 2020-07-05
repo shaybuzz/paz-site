95ee4270e6114f711fd0df906786ae516360d8b7
Shay Buzz <shaybuzz@gmail.com>
2020-07-05 17:12:18 +0300

    fullsite

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var images = [], index = 0, interval = 5000;
+var images = [], index = 0, interval = 3000;
 images[0] = "images/adv1.jpg";
 images[1] = "images/adv2.jpg";
 images[2] = "images/adv3.jpg";
